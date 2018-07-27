713e14efd09872902ddc099545247ca280f683b0
Pratima Rai <pratima.thulungrai@gmail.com>
2018-07-26 19:06:46 -0700

    Added example for function

---
+++ function/variable_scope.py
@@ -0,0 +1,26 @@
+# a = 10
+#
+#
+# def test_method(a):
+#     print('Value of local a is: ' + str(a))
+#     a = 2
+#     print('New value of local a is: ' + str(a))
+#
+#
+# print('Value of global a is: ' + str(a))
+# test_method(a)
+# print('Did the value of a change? ' + str(a))
+
+a = 10
+
+
+def test_method():
+    global a
+    print("Value of 'a' inside the method is: " + str(a))
+    a = 2
+    print("New value of 'a' is: " + str(a))
+
+
+print('Value of global a is: ' + str(a))
+test_method()
+print('Did the value of a change ? ' + str(a))
