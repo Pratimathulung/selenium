71020955cf7db7329a2d8d7cc4267ceedecd21bc
Pratima Rai <pratima.thulungrai@gmail.com>
2018-11-20 14:39:02 -0800

    Modified earlier files

--- selenium_basics/python_org_search_class.py
+++ selenium_basics/python_org_search_class.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -10,6 +12,15 @@ class PythonOrgSearch(unittest.TestCase):
     def test_python_org_search_with_invalid_word(self):
         self.driver.get("http://www.python.org")
         assert "Welcome to Python.org" == self.driver.title
+        time.sleep(2)
+
+        search_bar = self.driver.find_element_by_id('id-search-field')
+        search_bar.clear()
+        search_bar.send_keys('Pratima')
+        time.sleep(2)
+        search_bar.send_keys(Keys.RETURN)
+        assert 'No results found.' in self.driver.page_source
+        time.sleep(2)
 
     def tearDown(self):
         self.driver.close()
