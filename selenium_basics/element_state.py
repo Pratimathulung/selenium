a30258bea7c1c83a5723d54e57a28e712ea87762
Pratima Rai <pratima.thulungrai@gmail.com>
2018-11-28 14:30:26 -0800

    Added example to find the element state of web element

---
+++ selenium_basics/element_state.py
@@ -0,0 +1,32 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class ElementState(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_is_enabled(self):
+        self.driver.get('https://www.google.com/')
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
+
+        element = self.driver.find_element_by_name("q")
+        element_state = element.is_enabled()
+        print('Element is enabled?? ' + str(element_state))
+
+        if element_state is True:
+            element.send_keys('Coding is fun')
+            element.send_keys(Keys.RETURN)
+            time.sleep(2)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
